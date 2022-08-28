0aa10bd9def5f54776bf907f73562c9f9028cfdb
pauljuneau <juneaupaul3@gmail.com>
2022-08-27 21:59:02 -0700

    added items to collect

--- src/game.js
+++ src/game.js
@@ -45,6 +45,7 @@ var config = {
 };
 
 var player;
+var stars;
 var platforms;
 var cursors;
 
@@ -55,75 +56,106 @@ function preload ()
   this.load.setBaseURL('http://labs.phaser.io');
   this.load.image('sky', 'assets/skies/sky4.png');
   this.load.image('ground', 'assets/sprites/platform.png');
-  this.load.image('star', 'assets/sprites/star.png');
-  //this.load.image('bomb', 'assets/bomb.png');
+  // this.load.spritesheet(
+  //   {
+  //     key: 'star', 
+  //     url: 'assets/sprites/star.png',
+  //     frameConfig: {
+  //       frameWidth: 32,
+  //       frameHeight: 48
+  //     }
+  //   }
+  // );
+  this.load.image('star', 'assets/sprites/orb-red.png');
+  this.load.image('bomb', 'assets/sprites/spikedball.png');
   this.load.spritesheet('dude', 'assets/sprites/dude.png', { frameWidth: 32, frameHeight: 48 });
 }
 
 function create ()
 {
-    this.add.image(400, 300, 'sky');
+  this.add.image(400, 300, 'sky');
 
-    platforms = this.physics.add.staticGroup();
+  platforms = this.physics.add.staticGroup();
 
-    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+  platforms.create(400, 568, 'ground').setScale(2).refreshBody();
 
-    platforms.create(600, 400, 'ground');
-    platforms.create(50, 250, 'ground');
-    platforms.create(750, 220, 'ground');
+  platforms.create(600, 400, 'ground');
+  platforms.create(50, 250, 'ground');
+  platforms.create(750, 220, 'ground');
 
-    player = this.physics.add.sprite(100, 450, 'dude');
+  player = this.physics.add.sprite(100, 450, 'dude');
 
-    player.setBounce(0.2);
-    player.setCollideWorldBounds(true);
+  player.setBounce(0.2);
+  player.setCollideWorldBounds(true);
 
-    this.anims.create({
-        key: 'left',
-        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
-        frameRate: 10,
-        repeat: -1
-    });
+  this.anims.create({
+      key: 'left',
+      frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+      frameRate: 10,
+      repeat: -1
+  });
 
-    this.anims.create({
-        key: 'turn',
-        frames: [ { key: 'dude', frame: 4 } ],
-        frameRate: 20
-    });
+  this.anims.create({
+      key: 'turn',
+      frames: [ { key: 'dude', frame: 4 } ],
+      frameRate: 20
+  });
 
-    this.anims.create({
-        key: 'right',
-        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
-        frameRate: 10,
-        repeat: -1
-    });
+  this.anims.create({
+      key: 'right',
+      frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+      frameRate: 10,
+      repeat: -1
+  });
 
-    cursors = this.input.keyboard.createCursorKeys();
-    this.physics.add.collider(player, platforms);
+  cursors = this.input.keyboard.createCursorKeys();
+
+  stars = this.physics.add.group({
+      key: 'star',
+      repeat: 11,
+      setXY: { x: 12, y: 0, stepX: 70 }
+  });
+
+  stars.children.iterate(function (child) {
+
+      child.setBounceY(Phaser.Math.FloatBetween(0.1, 0.3));
+
+  });
+
+  this.physics.add.collider(player, platforms);
+  this.physics.add.collider(stars, platforms);
+
+  this.physics.add.overlap(player, stars, collectStar, null, this);
 }
 
 function update ()
 {
   if (cursors.left.isDown)
   {
-      player.setVelocityX(-160);
+    player.setVelocityX(-160);
 
-      player.anims.play('left', true);
+    player.anims.play('left', true);
   }
   else if (cursors.right.isDown)
   {
-      player.setVelocityX(160);
+    player.setVelocityX(160);
 
-      player.anims.play('right', true);
+    player.anims.play('right', true);
   }
   else
   {
-      player.setVelocityX(0);
+    player.setVelocityX(0);
 
-      player.anims.play('turn');
+    player.anims.play('turn');
   }
 
   if (cursors.up.isDown && player.body.touching.down)
   {
-      player.setVelocityY(-330);
+    player.setVelocityY(-330);
   }
+}
+
+function collectStar (player, star)
+{
+  star.disableBody(true, true);
 }
