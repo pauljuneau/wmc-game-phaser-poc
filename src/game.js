cb4eb25efd7428a96f98d85986df432e9bcf292e
pauljuneau <juneaupaul3@gmail.com>
2022-08-27 20:22:40 -0700

    adding in static platforms

--- src/game.js
+++ src/game.js
@@ -30,6 +30,13 @@ var config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
+  physics: {
+      default: 'arcade',
+      arcade: {
+          gravity: { y: 300 },
+          debug: false
+      }
+  },
   scene: {
       preload: preload,
       create: create,
@@ -42,16 +49,26 @@ var game = new Phaser.Game(config);
 function preload ()
 {
   this.load.setBaseURL('http://labs.phaser.io');
-  this.load.image('sky', 'assets/skies/sky1.png');
+  this.load.image('sky', 'assets/skies/sky4.png');
   this.load.image('ground', 'assets/sprites/platform.png');
   this.load.image('star', 'assets/sprites/star.png');
   //this.load.image('bomb', 'assets/bomb.png');
   this.load.spritesheet('dude', 'assets/sprites/dude.png', { frameWidth: 32, frameHeight: 48 });
 }
 
+var platforms;
+
 function create ()
 {
-  this.add.image(400, 300, 'sky');
+    this.add.image(400, 300, 'sky');
+
+    platforms = this.physics.add.staticGroup();
+
+    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+    platforms.create(600, 400, 'ground');
+    platforms.create(50, 250, 'ground');
+    platforms.create(750, 220, 'ground');
 }
 
 function update ()
