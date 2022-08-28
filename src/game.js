98c7957ab2e801c72b132df7c71af5ed736ba3bd
pauljuneau <juneaupaul3@gmail.com>
2022-08-27 19:51:54 -0700

    unable to load assets from folder due to CORS restriction with file://. Instead loading from labs.phaser.io.

--- src/game.js
+++ src/game.js
@@ -22,54 +22,38 @@ function resizeCanvasToScreen() {
   gameCanvas.style.height = height + 'px';
 };
 
-window.addEventListener('resize', resizeCanvasToScreen, false);
+//Turning off for now since all assets need to be resized and not just the canvas
+//window.addEventListener('resize', resizeCanvasToScreen, false);
 
 
 var config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
-  physics: {
-      default: 'arcade',
-      arcade: {
-          gravity: { y: 200 }
-      }
-  },
   scene: {
       preload: preload,
-      create: create
+      create: create,
+      update: update
   }
 };
 
 var game = new Phaser.Game(config);
 
-
 function preload ()
 {
   this.load.setBaseURL('http://labs.phaser.io');
-
-  this.load.image('sky', 'assets/skies/space3.png');
-  this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-  this.load.image('red', 'assets/particles/red.png');
+  this.load.image('sky', 'assets/skies/sky1.png');
+  this.load.image('ground', 'assets/sprites/platform.png');
+  this.load.image('star', 'assets/sprites/star.png');
+  //this.load.image('bomb', 'assets/bomb.png');
+  this.load.spritesheet('dude', 'assets/sprites/dude.png', { frameWidth: 32, frameHeight: 48 });
 }
 
 function create ()
 {
   this.add.image(400, 300, 'sky');
+}
 
-  var particles = this.add.particles('red');
-
-  var emitter = particles.createEmitter({
-      speed: 100,
-      scale: { start: 1, end: 0 },
-      blendMode: 'ADD'
-  });
-
-  var logo = this.physics.add.image(400, 100, 'logo');
-
-  logo.setVelocity(100, 200);
-  logo.setBounce(1, 1);
-  logo.setCollideWorldBounds(true);
-
-  emitter.startFollow(logo);
+function update ()
+{
 }
