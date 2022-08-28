f67167dd08179317d233d466d01471c177c6fb55
pauljuneau <juneaupaul3@gmail.com>
2022-08-27 20:43:25 -0700

    adding dude sprite

--- src/game.js
+++ src/game.js
@@ -44,6 +44,9 @@ var config = {
   }
 };
 
+var player;
+var platforms;
+
 var game = new Phaser.Game(config);
 
 function preload ()
@@ -56,8 +59,6 @@ function preload ()
   this.load.spritesheet('dude', 'assets/sprites/dude.png', { frameWidth: 32, frameHeight: 48 });
 }
 
-var platforms;
-
 function create ()
 {
     this.add.image(400, 300, 'sky');
@@ -69,6 +70,31 @@ function create ()
     platforms.create(600, 400, 'ground');
     platforms.create(50, 250, 'ground');
     platforms.create(750, 220, 'ground');
+
+    player = this.physics.add.sprite(100, 450, 'dude');
+
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'turn',
+        frames: [ { key: 'dude', frame: 4 } ],
+        frameRate: 20
+    });
+
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1
+    });
 }
 
 function update ()
