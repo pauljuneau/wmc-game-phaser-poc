b6610f1b1030b8b498369e7a6e7cdc188991b469
pauljuneau <juneaupaul3@gmail.com>
2022-08-28 20:39:18 -0700

    finalized player movement with midi input. Updated readMe.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # wmc-game-phaser-poc
-Phaser game engine POC with WebMidi Conductor
+Created [Phaser 3.55.2](https://newdocs.phaser.io/docs/3.55.2) [first game tutorial](https://phaser.io/tutorials/making-your-first-phaser-3-game/part1_) with player movement [WebMidi Conductor v2.5.1](https://github.com/pauljuneau/webmidi-conductor/tree/v2.5.1)
+
+Player movement alterations: 
+* Player avatar, 'dude', moves left or right if the player went up or down the register; respectively. 
+* Player avatar, 'dude', jumps when playing a triad or 7th chord
--- src/game.js
+++ src/game.js
@@ -1,6 +1,7 @@
+//Switch on webmidi music conductor engine to interpret midi in real time
 switchOnOffMusicalPerformance(100);
 
-
+//Resize game canvas to fit screen
 var gameCanvas;
 window.addEventListener('load', (event) => {
   gameCanvas = document.getElementsByTagName('canvas')[0];
@@ -14,11 +15,11 @@ function resizeCanvasToScreen() {
   var height;
 
   if (windowRatio < canvasRatio) {
-      height = window.innerHeight;
-      width = height / canvasRatio;
+    height = window.innerHeight;
+    width = height / canvasRatio;
   } else {
-      width = window.innerWidth;
-      height = width * canvasRatio;
+    width = window.innerWidth;
+    height = width * canvasRatio;
   }
 
   gameCanvas.style.width = width + 'px';
@@ -27,33 +28,34 @@ function resizeCanvasToScreen() {
 
 window.addEventListener('resize', resizeCanvasToScreen, false);
 
-
+//Phaser game config
 var config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
   physics: {
-      default: 'arcade',
-      arcade: {
-          gravity: { y: 300 },
-          debug: false
-      }
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 300 },
+      debug: false
+    }
   },
   scene: {
-      preload: preload,
-      create: create,
-      update: update
+    preload: preload,
+    create: create,
+    update: update
   }
 };
 
+//Global game objects
 var player;
-var stars;
+var orbs;
 var platforms;
-var cursors;
 var score = 0;
 var gameOver = false;
 var scoreText;
 
+//Phaser game implementation details for mandatory functions: preload(), create(), and update()
 var game = new Phaser.Game(config);
 
 function preload ()
@@ -61,8 +63,8 @@ function preload ()
   this.load.setBaseURL('http://labs.phaser.io');
   this.load.image('sky', 'assets/skies/sky4.png');
   this.load.image('ground', 'assets/sprites/platform.png');
-  this.load.image('star', 'assets/sprites/orb-red.png');
-  this.load.image('bomb', 'assets/sprites/skull.png');
+  this.load.image('orb', 'assets/sprites/orb-red.png');
+  this.load.image('skull', 'assets/sprites/skull.png');
   this.load.spritesheet('dude', 'assets/sprites/dude.png', { frameWidth: 32, frameHeight: 48 });
 }
 
@@ -84,50 +86,46 @@ function create ()
   player.setCollideWorldBounds(true);
 
   this.anims.create({
-      key: 'left',
-      frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
-      frameRate: 10,
-      repeat: -1
+    key: 'left',
+    frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+    frameRate: 10,
+    repeat: -1
   });
 
   this.anims.create({
-      key: 'turn',
-      frames: [ { key: 'dude', frame: 4 } ],
-      frameRate: 20
+    key: 'turn',
+    frames: [ { key: 'dude', frame: 4 } ],
+    frameRate: 20
   });
 
   this.anims.create({
-      key: 'right',
-      frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
-      frameRate: 10,
-      repeat: -1
+    key: 'right',
+    frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+    frameRate: 10,
+    repeat: -1
   });
 
-  cursors = this.input.keyboard.createCursorKeys();
-
-  stars = this.physics.add.group({
-      key: 'star',
-      repeat: 11,
-      setXY: { x: 12, y: 0, stepX: 70 }
+  orbs = this.physics.add.group({
+    key: 'orb',
+    repeat: 11,
+    setXY: { x: 12, y: 0, stepX: 70 }
   });
 
-  stars.children.iterate(function (child) {
-
-      child.setBounceY(Phaser.Math.FloatBetween(0.1, 0.3));
-
+  orbs.children.iterate(function (child) {
+    child.setBounceY(Phaser.Math.FloatBetween(0.1, 0.3));
   });
 
-  bombs = this.physics.add.group();
+  skulls = this.physics.add.group();
 
   scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
 
   this.physics.add.collider(player, platforms);
-  this.physics.add.collider(stars, platforms);
-  this.physics.add.collider(bombs, platforms);
+  this.physics.add.collider(orbs, platforms);
+  this.physics.add.collider(skulls, platforms);
 
-  this.physics.add.overlap(player, stars, collectStar, null, this);
+  this.physics.add.overlap(player, orbs, collectOrb, null, this);
 
-  this.physics.add.collider(player, bombs, hitBomb, null, this);
+  this.physics.add.collider(player, skulls, hitSkull, null, this);
 }
 
 function update ()
@@ -136,54 +134,36 @@ function update ()
   {
     return;
   }
-  
-  if (cursors.left.isDown || (noteObjectOnByMidiNoteNumber.size > 0 && midiChlorianCtrlr.countDecreased))
-  {
-    player.setVelocityX(-160);
 
-    player.anims.play('left', true);
-  }
-  else if (cursors.right.isDown || (noteObjectOnByMidiNoteNumber.size > 0 && midiChlorianCtrlr.countIncreased))
-  {
-    player.setVelocityX(160);
-
-    player.anims.play('right', true);
-  }
-  else
-  {
-    player.setVelocityX(0);
-
-    player.anims.play('turn');
-  }
-
-  if ((cursors.up.isDown || musicConductor.chordsPlaying.length  > 0) && player.body.touching.down)
+  if ((musicConductor.chordsPlaying.length  > 0) && player.body.touching.down)
   {
     player.setVelocityY(-330);
   }
 }
 
-function collectStar (player, star)
+//Game behavior functions
+function collectOrb (player, orb)
 {
-  star.disableBody(true, true);
+  orb.disableBody(true, true);
   score += 10;
   scoreText.setText('Score: ' + score);
 
-  if (stars.countActive(true) === 0)
+  if (orbs.countActive(true) === 0)
   {
-    //  A new batch of stars to collect
-    stars.children.iterate(function (child) {
+    //  A new batch of orbs to collect
+    orbs.children.iterate(function (child) {
       child.enableBody(true, child.x, 0, true, true);
     });
     var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
-    var bomb = bombs.create(x, 16, 'bomb');
-    bomb.setBounce(1);
-    bomb.setCollideWorldBounds(true);
-    bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
-    bomb.allowGravity = false;
+    var skull = skulls.create(x, 16, 'skull');
+    skull.setBounce(1);
+    skull.setCollideWorldBounds(true);
+    skull.setVelocity(Phaser.Math.Between(-200, 200), 20);
+    skull.allowGravity = false;
   }
 }
 
-function hitBomb (player, bomb)
+function hitSkull (player, skull)
 {
   this.physics.pause();
 
@@ -195,11 +175,11 @@ function hitBomb (player, bomb)
 }
 
 
-
+//Player movement via midi input
 /** 
  * @description listens to midi-chlorian controller event which are events based on 
  * midi notes currently playing.
- * - moves player left or right if the player went up or down the register. 
+ * - moves player left or right if the player went up or down the register; respectively. 
  * @listens MidiInstrumentationEvents.MIDICHLORIANCTRLEVENT - published from
  *  midiChlorianController.js' MidiInstrumentationEvents.NOTEBEINGPLAYED event listener.
  * @param e.value - stringified midiChlorianCtrlr 
@@ -207,53 +187,47 @@ function hitBomb (player, bomb)
  */
  document.addEventListener(MidiInstrumentationEvents.MIDICHLORIANCTRLEVENT, function(e) {
   const oneMidiChlorianCtrlrEvent = JSON.parse(e.value);
-  var midiNoteNumber = Number(oneMidiChlorianCtrlrEvent.midiInputPlaying.note);
-  //Play Piano Sounds when playing on computer keyboardw
+  //Play Piano Sounds when playing on computer keyboard
   if(oneMidiChlorianCtrlrEvent.midiInputPlaying.eventType == 'KEYBOARD') {
-      try {
-          document.getElementById(oneMidiChlorianCtrlrEvent.midiInputPlaying.noteName).play();
-      } catch (e) {
-          console.error(e.name + ': '+e.message);
-      }
+    try {
+        document.getElementById(oneMidiChlorianCtrlrEvent.midiInputPlaying.noteName).play();
+    } catch (e) {
+        console.error(e.name + ': '+e.message);
+    }
   }
-  // try {
-  //     if(oneMidiChlorianCtrlrEvent.midiInputPlaying.command == 144) {
-  //         if(oneMidiChlorianCtrlrEvent.countIncreased) {
-  //           player.setVelocityX(500);
-  //           player.anims.play('right', true);
-  //         } else if ( oneMidiChlorianCtrlrEvent.countDecreased ) {
-  //           player.setVelocityX(-500);
-  //           player.anims.play('left', true);
-  //         } else {
-  //           player.setVelocityX(0);
-  //           player.anims.play('turn');
-  //         }
-  //     }
-  // } catch(e) {
-  //     console.error(e.name + ': '+e.message + "; stack: "+e.stack);
-  // }
-  // setTimeout(
-  //     function() {
-  //       if(musicConductor.chordsPlaying.length  == 0) {
-  //         player.setVelocityX(0);
-  //         player.anims.play('turn');
-  //       }
-  //     }, 
-  //     //hardcoding beat duration for .25 second for now... assuming 4 4 time
-  //     //TODO idea: use beat duration instead to cause affect on object to persist while note was held down
-  //     250
-  // );
+  //moves player left or right if the player went up or down the register; respectively. 
+  try {
+    if(oneMidiChlorianCtrlrEvent.countIncreased) {
+      player.setVelocityX(160);
+      player.anims.play('right', true);
+    } else if ( oneMidiChlorianCtrlrEvent.countDecreased ) {
+      player.setVelocityX(-160);
+      player.anims.play('left', true);
+    } else {
+      player.setVelocityX(0);
+      player.anims.play('turn');
+    }
+  } catch(e) {
+    console.error(e.name + ': '+e.message + "; stack: "+e.stack);
+  }
+  //Only move player for a quarter of a second (assuming 4 4 time)
+  setTimeout(
+    function() {
+      player.setVelocityX(0);
+      player.anims.play('turn');
+    },
+    250
+  );
 });
 
 //Stop piano sound being played when no longer playing using computer keyboard
 document.addEventListener(MidiInstrumentationEvents.NOTELASTPLAYED, function(e){
   const oneNoteLastPlayed = JSON.parse(e.value);
-  var midiNoteNumber = Number(oneNoteLastPlayed.note);
   if(oneNoteLastPlayed.eventType == 'KEYBOARD') {
-      try {
-          document.getElementById(oneNoteLastPlayed.noteName).pause();
-      } catch (e) {
-          console.error(e.name + ': '+e.message);
-      }
+    try {
+      document.getElementById(oneNoteLastPlayed.noteName).pause();
+    } catch (e) {
+      console.error(e.name + ': '+e.message);
+    }
   }
 });
